1bc61bb5cae23f112651e49225464d0580b79946
Matt Stratton <matt.stratton@gmail.com>
2013-11-15 20:35:35 -0600

    Initial commit

---
+++ lib/vagrant/azure.rb
@@ -0,0 +1,7 @@
+require "vagrant/azure/version"
+
+module Vagrant
+  module Azure
+    # Your code goes here...
+  end
+end
---
+++ lib/vagrant/azure/version.rb
@@ -0,0 +1,5 @@
+module Vagrant
+  module Azure
+    VERSION = "0.0.1"
+  end
+end
